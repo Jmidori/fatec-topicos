a227bc1894621d4cb6392839e542bec1e9799460
Juliane Midori Toda <jmidori.toda@gmail.com>
2020-10-24 12:11:28 -0300

    implentar consumidor da fila e produtor do topico

--- avaliacao/src/main/java/com/escola/avaliacao/servico/AtividadeConsumer.java
+++ avaliacao/src/main/java/com/escola/avaliacao/servico/AtividadeConsumer.java
@@ -4,7 +4,6 @@ import com.escola.avaliacao.entidade.Atividade;
 import com.escola.avaliacao.entidade.Resultado;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
@@ -12,28 +11,19 @@ import java.util.Random;
 @Component
 public class AtividadeConsumer {
     @Autowired
-    private JmsTemplate template;
+    ResultadoProducer producer;
 
-    @JmsListener(destination = "${jms.queue}", containerFactory = "jmsFactory")
-    public void getAtividadeEnfileirada(Atividade atividade) {
-        System.out.println("-- Atividade recebida: "+ atividade.toString());
+    @JmsListener(destination = "${jms.queue}", containerFactory = "consumerFactory")
+    public void recebeMessagem(Atividade atividade){
+        System.out.println("-- Atividade: " + atividade.toString());
 
+//        producer.enfileiraTopico(this.getResultado(atividade));
         Resultado resultado = this.getResultado(atividade);
-        System.out.println("-- Resultado enviado ao topico: " + resultado);
-
-
-
-
-
-            //envia para o topico o resultado
-            //salva no db
-            return;
-
-        //log erro
-
+        producer.enfileiraTopico(resultado);
+        System.out.println("-- Resultado: " + resultado);
     }
 
-    private Resultado getResultado(Atividade atividade) {
+    public Resultado getResultado(Atividade atividade) {
         Random random = new Random();
 
         return new Resultado(atividade.getNome(),
--- avaliacao/src/main/java/com/escola/avaliacao/servico/ResultadoProducer.java
+++ avaliacao/src/main/java/com/escola/avaliacao/servico/ResultadoProducer.java
@@ -1,8 +1,20 @@
 package com.escola.avaliacao.servico;
 
+import com.escola.avaliacao.entidade.Resultado;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ResultadoProducer {
-    //todo: continuar daqui
+    @Autowired
+    JmsTemplate jmsTemplate;
+
+    @Value("${jms.topic}")
+    String topic;
+
+    public void enfileiraTopico(Resultado resultado){
+        jmsTemplate.convertAndSend(topic,resultado);
+    }
 }
